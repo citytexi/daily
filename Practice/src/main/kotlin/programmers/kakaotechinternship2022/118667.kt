0218e24051f5fb3356d0d044d1e686efe5ff8920
citytexi <heekyne66@gmail.com>
2023-03-29 08:25:43 +0900

    init practice

---
+++ Practice/src/main/kotlin/programmers/kakaotechinternship2022/118667.kt
@@ -0,0 +1,17 @@
+package programmers.kakaotechinternship2022
+
+private class Solution118667 {
+
+    fun solution(
+        queue1: IntArray,
+        queue2: IntArray
+    ): Int {
+        var answer: Int = -2
+        return answer
+    }
+
+}
+
+private fun main() {
+
+}
