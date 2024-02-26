e3b37ea42825ce415ad33fca57d54efe37d3bdef
citytexi <heekyne66@gmail.com>
2024-02-26 13:01:52 +0900

    프로그래머스 lv2

---
+++ Practice/src/main/kotlin/programmers/lv2/Solution42883.kt
@@ -0,0 +1,66 @@
+package programmers.lv2
+
+/*
+문제 설명
+어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
+
+문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+제한 조건
+number는 2자리 이상, 1,000,000자리 이하인 숫자입니다.
+k는 1 이상 number의 자릿수 미만인 자연수입니다.
+
+입출력 예
+number	k	return
+"1924"	2	"94"
+"1231234"	3	"3234"
+"4177252841"	4	"775841"
+* */
+
+private class Solution42883 {
+    fun solution(number: String, k: Int): String {
+        val answer = mutableListOf<Char>()
+        var index = 0
+        val size = number.length - k
+
+        for(i in 0 until size){
+            var max = '0'
+
+            val lastIndex = i + k
+            for (j in index .. lastIndex) {
+                if (max < number[j]) {
+                    max = number[j]
+                    index = j + 1
+                }
+            }
+
+            answer.add(max)
+        }
+
+        return answer.joinToString("")
+    }
+}
+
+private fun main() {
+    val solution42883 = Solution42883()
+    println(
+        solution42883.solution(
+            number = "1924",
+            k = 2
+        )
+    )
+    println(
+        solution42883.solution(
+            number = "1231234",
+            k = 3
+        )
+    )
+    println(
+        solution42883.solution(
+            number = "4177252841",
+            k = 4
+        )
+    )
+}
