0fd2566c245e76c4f411bad6f35a9ed852e87058
citytexi <heekyne66@gmail.com>
2024-03-31 13:51:49 +0900

    안드로이드 스터디 프로젝트

---
+++ AndroidPractice/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,8 @@
+
+n
+>app/src/main/java/com/citytexi/androidpractice/MainActivity.kt,e/e/ee4a7504f80bd313af0800c5ee8840eeb2dc5dcd
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ AndroidPractice/app/src/main/java/com/citytexi/androidpractice/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.citytexi.androidpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.citytexi.androidpractice.ui.theme.AndroidPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidPracticeTheme {
+        Greeting("Android")
+    }
+}
